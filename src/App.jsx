41bd55d47901fe3c6aa2e575cc36f8781b6a2e5a
David <david.gutierrez.dinares@juanbosco.cl>
2025-09-08 19:24:38 -0300

    adding the styling files and some basic structural code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import "./app.css";
 import Chart from "./components/chart";
 import data from "./data/data.json";
 
@@ -9,7 +9,7 @@ function App() {
   const arrayOfCharts = data.map((chart, index) => (
     <Chart
       key={index}
-      img={chart.image.thumbnail}
+      img={chart.image.mobile}
       name={chart.name}
       price={chart.price}
       category={chart.category}
@@ -19,15 +19,19 @@ function App() {
 
   return (
     <>
-      <header>Desserts</header>
-      {arrayOfCharts}
-      <aside className="shopping-cart">
-        <h1>Your Cart {/* The number of shop items */}</h1>
-        {/* the array of components derived from the useState */}
-        {/* if the code above its true, then render a <p>Order Total ${price}</p> */}
-        {/* If the thing above is true (more than 0), then add this text "This is a carbon-neutral delivery" */}
-        <button>Corfirm order</button>
-      </aside>
+      <header>
+        <h1>Desserts</h1>
+      </header>
+      <main>
+        {arrayOfCharts}
+        <aside className="shopping-cart">
+          <h1>Your Cart {/* The number of shop items */}</h1>
+          {/* the array of components derived from the useState */}
+          {/* if the code above its true, then render a <p>Order Total ${price}</p> */}
+          {/* If the thing above is true (more than 0), then add this text "This is a carbon-neutral delivery" */}
+          <button>Corfirm order</button>
+        </aside>
+      </main>
     </>
   );
 }
--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -2,15 +2,17 @@ import AddToCartIcon from "../icons/icon-add-to-cart.svg";
 
 export default function Chart(props) {
   return (
-    <section id={props.key}>
+    <section className="dessert" id={props.key}>
       <img src={props.img} alt={`The image of a ${props.name}`} />
-      <h3>{props.category}</h3>
-      <h1>{props.name}</h1>
-      <h3>{props.price}</h3>
-      <button>
+
+      {/* <button>
         <img src={AddToCartIcon} />
         Add to Cart
-      </button>
+      </button> */}
+      {/*I need to replace this for an button input*/}
+      <small>{props.category}</small>
+      <h3>{props.name}</h3>
+      <p>{props.price}</p>
     </section>
   );
 }
