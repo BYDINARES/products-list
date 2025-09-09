24f84fc975d201c6e7b7832fff7486d21b95e276
David <david.gutierrez.dinares@juanbosco.cl>
2025-09-09 06:46:19 -0300

    placing the button under the image and the addign some basic stylign to the aside container

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import "./app.css";
 import Chart from "./components/chart";
 import data from "./data/data.json";
+import ilustrationEmptyCart from "./icons/illustration-empty-cart.svg";
 
 function App() {
-  const [shopItems, setShopItems] = useState(null);
+  const [shopItems, setShopItems] = useState([]);
 
   const arrayOfCharts = data.map((chart, index) => (
     <Chart
@@ -25,11 +26,20 @@ function App() {
       <main>
         {arrayOfCharts}
         <aside className="shopping-cart">
-          <h1>Your Cart {/* The number of shop items */}</h1>
+          <h1>Your Cart ({shopItems.length})</h1>
           {/* the array of components derived from the useState */}
           {/* if the code above its true, then render a <p>Order Total ${price}</p> */}
           {/* If the thing above is true (more than 0), then add this text "This is a carbon-neutral delivery" */}
-          <button>Corfirm order</button>
+          {shopItems.length == 0 && (
+            <img
+              className="ilustration-empty-cart"
+              src={ilustrationEmptyCart}
+            ></img>
+          )}
+
+          {shopItems.length == 0 && <p>Your added items will appear here</p>}
+
+          {shopItems.length > 0 && <button>Corfirm order</button>}
         </aside>
       </main>
     </>
--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -5,11 +5,11 @@ export default function Chart(props) {
     <section className="dessert" id={props.key}>
       <img src={props.img} alt={`The image of a ${props.name}`} />
 
-      {/* <button>
+      <button>
         <img src={AddToCartIcon} />
         Add to Cart
-      </button> */}
-      {/*I need to replace this for an button input*/}
+      </button>
+
       <small>{props.category}</small>
       <h3>{props.name}</h3>
       <p>{props.price}</p>
